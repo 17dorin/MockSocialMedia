1438cb9e1beab865617f21399d9ee7e1161a9753
Nick D'Oria <17dorin@gmail.com>
2021-04-16 17:00:21 -0400

    Initial project set up

---
+++ Models/Post.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace MockSocialMedia.Models
+{
+    public partial class Post
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public int? Likes { get; set; }
+        public string Poster { get; set; }
+
+        public virtual AspNetUser PosterNavigation { get; set; }
+    }
+}

316e50962ad1930401c66e0ddaf444622d7be39b
Nick D'Oria <17dorin@gmail.com>
2021-04-30 16:10:55 -0400

    Added a home feed showing posts based on users you are following

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,26 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MockSocialMedia.Models;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace MockSocialMedia.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly MockSocialMediaContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(MockSocialMediaContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            //Gets current user id
+            string currentUser = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            //Gets a list of posts based on the users the current user is following
+            List<Post> yourHomeFeed = new List<Post>();
+            List<string> followedUserIds = _context.FollowedUsers.Where(x => x.FollowingUser == currentUser).Select(x => x.UserToFollow).ToList();
+            yourHomeFeed = _context.Posts.Where(x => followedUserIds.Contains(x.Poster)).ToList();
+
+            return View(yourHomeFeed);
         }
 
         public IActionResult Privacy()
